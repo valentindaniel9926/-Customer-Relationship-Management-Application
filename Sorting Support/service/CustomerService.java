ad169f7be42f055c745eeb27c6fd1341bd45218e
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2022-02-07 10:26:03 +0200

    Add files via upload

---
+++ Sorting Support/service/CustomerService.java
@@ -0,0 +1,18 @@
+package com.luv2code.springdemo.service;
+
+import java.util.List;
+
+import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.util.SortUtils;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers(int theSortField);
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
